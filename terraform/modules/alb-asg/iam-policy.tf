3339062d3de25e5f7b35dacbab2ba90fd7f1556d
bedilk <128605295+bedilk@users.noreply.github.com>
2024-05-22 18:09:03 +0200

    Update iam-policy.tf

--- terraform/modules/alb-asg/iam-policy.tf
+++ terraform/modules/alb-asg/iam-policy.tf
@@ -4,7 +4,7 @@ resource "aws_iam_role" "iam_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
+        Action = "*"
         Effect = "Allow"
         Principal = {
           Service = "ec2.amazonaws.com"
